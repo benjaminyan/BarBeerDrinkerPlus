564d8232d396c0f246a870b90e16d536060b0a3b
benjaminyan <by120@scarletmail.rutgers.edu>
2018-11-21 15:40:40 -0500

    sql query improvements

--- bbd-ui/src/app/sqlquery/sqlquery.component.ts
+++ bbd-ui/src/app/sqlquery/sqlquery.component.ts
@@ -10,21 +10,25 @@ export class SQLQueryComponent implements OnInit {
   sqlQuery: string;
   columns: [];
   rows: [];
-  errorMessage: string;
+  message: string;
   constructor(private dataService: DataService) { }
 
   runClicked() {
     const obs = this.dataService.getResultFromQuery(this.sqlQuery);
 
     obs.subscribe((data: any) => {
-      if (data['error'] == null) {
+
+      if (data['error'] !== null) {
+        this.message = data['error'];
+      }
+      else if (data['result'] !== null) {
+        this.message = data['result'];
+      }
+      else {
         this.rows = data['rows'];
         this.columns = data['colNames'].map((colName: string) => {
           return {header: colName, field: colName};
         });
-      }
-      else {
-        this.errorMessage = data['error'];
       }
       console.log(this.rows);
     });
